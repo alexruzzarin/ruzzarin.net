c2f185b9a3d9bb1a1eaed908cea97163aba06662
Alex Ruzzarin <alex@ruzzarin.net>
2014-10-13 01:15:29 -0300

    add fallback

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,196 +1,195 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  var appConfig = {
-    src: {
-      js: ['app/script/**/*.js'],
-      css: ['app/style/**/*.less'],
-      image: ['app/image/**/*.*']
-    },
-    vendor: {
-      js: ['bower_components/jquery/dist/jquery.js',
-           'bower_components/bootstrap/dist/js/bootstrap.js'],
-      css: ['bower_components/bootstrap/dist/css/bootstrap.css',
-            'bower_components/Font-Awesome/css/font-awesome.css'],
-      font: ['bower_components/bootstrap/dist/fonts/**',
-             'bower_components/Font-Awesome/fonts/**']
-    }
-  };
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    ect: {
-      app: {
-        options: {
-          variables: {
-            env: process.env.NODE_ENV,
-            name: "Alex Ruzzarin",
-            title: "Alex Ruzzarin - Software Developer",
-            description: "Programmer with degree in Analysis and Development of Information Systems. Professional and trainer certified by Microsoft. Crazy for technology, works with. Net, but is a fan of Ruby and JavaScript (and Node.Js).",
-            image: "http://www.ruzzarin.net/images/Alex-Ruzzarin.jpg",
-            keywords: "HTML,CSS,JavaScript,.net,dotnet,node,nodejs,node.js,asp.net,aspnet",
-            googlePlusUrl: "https://plus.google.com/+AlexRuzzarin/posts"
-          }
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        ect: {
+            appHtml: {
+                options: {
+                    variables: {
+                        env: process.env.NODE_ENV,
+                        name: "Alex Ruzzarin",
+                        title: "Alex Ruzzarin - Software Developer",
+                        description: "Programmer with degree in Analysis and Development of Information Systems. Professional and trainer certified by Microsoft. Crazy for technology, works with. Net, but is a fan of Ruby and JavaScript (and Node.Js).",
+                        image: "http://www.ruzzarin.net/images/Alex-Ruzzarin.jpg",
+                        keywords: "HTML,CSS,JavaScript,.net,dotnet,node,nodejs,node.js,asp.net,aspnet",
+                        googlePlusUrl: "https://plus.google.com/+AlexRuzzarin/posts"
+                    }
+                },
+                files: {
+                    'public/index.html': 'app/index.ect'
+                }
+            }
         },
-        files: {
-          'public/index.html': 'app/index.ect',
-        }
-      }
-    },
-    htmlhint: {
-      app: {
-        options: {
-            'tag-pair': true,
-            'tagname-lowercase': true,
-            'attr-lowercase': true,
-            'attr-value-double-quotes': true,
-            'doctype-first': true,
-            'spec-char-escape': true,
-            'id-unique': true,
-            'head-script-disabled': true,
-            'style-disabled': true
+        htmlhint: {
+            appHtml: {
+                options: {
+                    'tag-pair': true,
+                    'tagname-lowercase': true,
+                    'attr-lowercase': true,
+                    'attr-value-double-quotes': true,
+                    'doctype-first': true,
+                    'spec-char-escape': true,
+                    'id-unique': true,
+                    'head-script-disabled': true,
+                    'style-disabled': true
+                },
+                src: 'public/index.html'
+            }
         },
-        src: 'public/index.html'
-      }
-    },
-    htmlmin: {
-      app: {
-        options: {
-          removeComments: true,
-          collapseWhitespace: true
+        htmlmin: {
+            appHtml: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'public/index.html': 'public/index.html'
+                }
+            }
         },
-        files: {
-          'public/index.html': 'public/index.html'
-        }
-      }
-    },
-    jshint: {
-      // You get to make the name
-      // The paths tell JSHint which files to validate
-      app: appConfig.src.js
-    },
-    uglify: {
-      app: {
-        files: {
-          'public/script/app.js': appConfig.src.js
-        }
-      },
-      vendor: {
-        files: {
-          'public/script/vendor.js': appConfig.vendor.js
-        }
-      }
-    },
-    less: {
-      app: {
-        src: appConfig.src.css,
-        dest: 'public/style/app.css'
-      }
-    },
-    cssc: {
-      app: {
-        options: {
-          consolidateViaDeclarations: true,
-          consolidateViaSelectors: true,
-          consolidateMediaQueries: true
+        jshint: {
+            // You get to make the name
+            // The paths tell JSHint which files to validate
+            appJs: ['app/script/**/*.js']
         },
-        files: {
-            'public/style/app.css': 'public/style/app.css'
+        concat:{
+            appJs: {
+                src: ['app/script/**/*.js'],
+                dest: 'public/js/app.js'
+            }
+        },
+        uglify: {
+            appJs: {
+                files: {
+                    'public/js/app.js': ['public/js/app.js']
+                }
+            }
+        },
+        less: {
+            appCss: {
+                src: ['app/style/**/*.less'],
+                dest: 'public/css/app.css'
+            }
+        },
+        cssc: {
+            appCss: {
+                options: {
+                    consolidateViaDeclarations: true,
+                    consolidateViaSelectors: true,
+                    consolidateMediaQueries: true
+                },
+                files: {
+                    'public/css/app.css': 'public/css/app.css'
+                }
+            }
+        },
+        cssmin: {
+            appCss: {
+                src: 'public/css/app.css',
+                dest: 'public/css/app.css'
+            }
+        },
+        copy: {
+            vendorFonts: {
+                files: [
+                    {expand: true, src: ['bower_components/bootstrap/dist/fonts/**',
+                        'bower_components/Font-Awesome/fonts/**'], dest: 'public/fonts/', flatten: true, filter: 'isFile'}
+                ]
+            },
+            vendorCss: {
+                files: [
+                    {src: 'bower_components/bootstrap/dist/css/bootstrap.min.css', dest: 'public/css/bootstrap.min.css'},
+                    {src: 'bower_components/Font-Awesome/css/font-awesome.min.css', dest: 'public/css/font-awesome.min.css'}
+                ]
+            },
+            vendorJs: {
+                files: [
+                    {src: 'bower_components/fallback/fallback.min.js', dest: 'public/js/fallback.min.js'}
+                ]
+            },
+            appImages: {
+                files: [
+                    {expand: true, src: ['app/images/**/*.*'], dest: 'public/images/', flatten: true, filter: 'isFile'}
+                ]
+            }
+        },
+        clean: {
+            options: {
+                force: true
+            },
+            dest: {
+                src: ["public/**"]
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            html: {
+                files: ['app/index.ect'],
+                tasks: ['appHtml'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['app/script/**/*.js'],
+                tasks: ['appJs'],
+                options: {
+                    livereload: true
+                }
+            },
+            css: {
+                files: ['app/style/**/*.less'],
+                tasks: ['appCss'],
+                options: {
+                    livereload: true
+                }
+            },
+            image: {
+                files: ['app/images/**/*.*'],
+                tasks: ['appImages'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        connect: {
+            app: {
+                options: {
+                    port: 9000,
+                    base: 'public'
+                }
+            }
         }
-      }
-    },
-    cssmin: {
-      app: {
-        src: 'public/style/app.css',
-        dest: 'public/style/app.css'
-      },
-      vendor: {
-        files: {
-          'public/style/vendor.css': appConfig.vendor.css
-        }
-      }
-    },
-    copy: {
-      vendor: {
-        files: [
-          {expand: true, src: appConfig.vendor.font, dest: 'public/fonts/', flatten: true, filter: 'isFile'}
-        ]
-      },
-      app: {
-        files: [
-          {expand: true, src: appConfig.src.image, dest: 'public/image/', flatten: true, filter: 'isFile'},
-        ]
-      }
-    },
-    clean: {
-      options: {
-        force: true
-      },
-      app: {
-        src: ["public/**"]
-      }
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      html: {
-        files: ['app/index.ect'],
-        tasks: ['appHtml'],
-        options: {
-          livereload: true,
-        }
-      },
-      js: {
-        files: appConfig.src.js,
-        tasks: ['appJs'],
-        options: {
-          livereload: true,
-        }
-      },
-      css: {
-        files: appConfig.src.css,
-        tasks: ['appCss'],
-        options: {
-          livereload: true,
-        }
-      },
-      image: {
-        files: appConfig.src.image,
-        tasks: ['appImage'],
-        options: {
-          livereload: true,
-        }
-      }
-    },
-    connect: {
-      app: {
-        options: {
-          port: 9000,
-          base: 'public'
-        }
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('appHtml', ['ect:app','htmlhint:app','htmlmin:app']);
-  grunt.registerTask('appCss', ['less:app','cssc:app','cssmin:app']);
-  grunt.registerTask('appJs', ['jshint:app','uglify:app']);
-  grunt.registerTask('appImage', ['copy:app']);
+    grunt.registerTask('appHtml', ['ect:appHtml', 'htmlhint:appHtml']);
+    grunt.registerTask('appCss', ['less:appCss']);
+    grunt.registerTask('appJs', ['jshint:appJs', 'concat:appJs']);
+    grunt.registerTask('appImages', ['copy:appImages']);
 
-  grunt.registerTask('vendorCss', ['cssmin:vendor']);
-  grunt.registerTask('vendorJs', ['uglify:vendor']);
-  grunt.registerTask('vendorFont', ['copy:vendor']);
+    grunt.registerTask('releaseAppHtml', ['appHtml', 'htmlmin:appHtml']);
+    grunt.registerTask('releaseAppCss', ['appCss', 'cssc:appCss', 'cssmin:appCss']);
+    grunt.registerTask('releaseAppJs', ['appJs', 'uglify:appJs']);
+    grunt.registerTask('releaseAppImages', ['appImages']);
 
-  grunt.registerTask('default', ['clean','appHtml','appCss','appJs','appImage','vendorCss','vendorJs','vendorFont']);
+    grunt.registerTask('vendorCss', ['copy:vendorCss']);
+    grunt.registerTask('vendorJs', ['copy:vendorJs']);
+    grunt.registerTask('vendorFont', ['copy:vendorFonts']);
 
-  grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
-    grunt.task.run([
-      'default',
-      'connect:app',
-      'watch'
-    ]);
-  });
+    grunt.registerTask('default', ['clean', 'appHtml', 'appCss', 'appJs', 'appImages', 'vendorCss', 'vendorJs', 'vendorFont']);
+
+    grunt.registerTask('release', ['clean', 'releaseAppHtml', 'releaseAppCss', 'releaseAppJs', 'releaseAppImages', 'vendorCss', 'vendorJs', 'vendorFont']);
+
+    grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
+        grunt.task.run([
+            'default',
+            'connect:app',
+            'watch'
+        ]);
+    });
 };
