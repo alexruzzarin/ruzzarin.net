59481fd5418cd21228d4f4dd9c28cbfa6e22279b
Alex Ruzzarin <alex@ruzzarin.net>
2014-12-17 00:22:47 -0300

    azure cdn deploy + codeship

--- Gruntfile.js
+++ Gruntfile.js
@@ -176,6 +176,72 @@ module.exports = function (grunt) {
                 src: ["public/**"]
             }
         },
+        'azure-cdn-deploy': {
+            appJs: {
+                options: {
+                    containerName: 'jss', // container name in blob
+                    serviceOptions: [process.env.AZURE_BLOB_NAME, process.env.AZURE_BLOB_NAME], // custom arguments to azure.createBlobService
+                    folder: '', // path within container
+                    zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+                    deleteExistingBlobs: true, // true means recursively deleting anything under folder
+                    concurrentUploadThreads: 10, // number of concurrent uploads, choose best for your network condition
+                    metadata: {
+                        cacheControl: 'public, max-age=31530000', // cache in browser
+                        cacheControlHeader: 'public, max-age=31530000' // cache in azure CDN. As this data does not change, we set it to 1 year
+                    },
+                    testRun: false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+                },
+                src: ['/public/jss/app.js']
+            },
+            appCss: {
+                options: {
+                    containerName: 'jss', // container name in blob
+                    serviceOptions: [process.env.AZURE_BLOB_NAME, process.env.AZURE_BLOB_NAME], // custom arguments to azure.createBlobService
+                    folder: '', // path within container
+                    zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+                    deleteExistingBlobs: true, // true means recursively deleting anything under folder
+                    concurrentUploadThreads: 10, // number of concurrent uploads, choose best for your network condition
+                    metadata: {
+                        cacheControl: 'public, max-age=31530000', // cache in browser
+                        cacheControlHeader: 'public, max-age=31530000' // cache in azure CDN. As this data does not change, we set it to 1 year
+                    },
+                    testRun: false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+                },
+                src: ['/public/css/app.css']
+            },
+            appFonts: {
+                options: {
+                    containerName: 'jss', // container name in blob
+                    serviceOptions: [process.env.AZURE_BLOB_NAME, process.env.AZURE_BLOB_NAME], // custom arguments to azure.createBlobService
+                    folder: '', // path within container
+                    zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+                    deleteExistingBlobs: true, // true means recursively deleting anything under folder
+                    concurrentUploadThreads: 10, // number of concurrent uploads, choose best for your network condition
+                    metadata: {
+                        cacheControl: 'public, max-age=31530000', // cache in browser
+                        cacheControlHeader: 'public, max-age=31530000' // cache in azure CDN. As this data does not change, we set it to 1 year
+                    },
+                    testRun: false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+                },
+                src: ['/public/fonts/ruzzarin.*']
+            },
+            appImages: {
+                options: {
+                    containerName: 'jss', // container name in blob
+                    serviceOptions: [process.env.AZURE_BLOB_NAME, process.env.AZURE_BLOB_NAME], // custom arguments to azure.createBlobService
+                    folder: '', // path within container
+                    zip: true, // gzip files if they become smaller after zipping, content-encoding header will change if file is zipped
+                    deleteExistingBlobs: true, // true means recursively deleting anything under folder
+                    concurrentUploadThreads: 10, // number of concurrent uploads, choose best for your network condition
+                    metadata: {
+                        cacheControl: 'public, max-age=31530000', // cache in browser
+                        cacheControlHeader: 'public, max-age=31530000' // cache in azure CDN. As this data does not change, we set it to 1 year
+                    },
+                    testRun: false // test run - means no blobs will be actually deleted or uploaded, see log messages for details
+                },
+                src: ['/public/images/**/*.*']
+            }
+        },
         watch: {
             options: {
                 livereload: true
